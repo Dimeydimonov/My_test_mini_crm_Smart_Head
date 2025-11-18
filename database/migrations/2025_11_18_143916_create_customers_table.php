afabeb8af7f47b0d85d348a33704ba9688a0df2f
Dimeydimonov <Dimeydimonov1@gmail.com>
2025-11-18 17:20:16 +0200

    созданы и мигрированы таблицы в БД Customers, Tickets

---
+++ database/migrations/2025_11_18_143916_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table-> string ('name');
+            $table-> string ('phone_number') ->unique()->comment('E.164 format');
+            $table-> string ('email') ->unique();
+			$table->timestamps();
+
+			$table->index('phone_number');
+			$table->index('email');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
