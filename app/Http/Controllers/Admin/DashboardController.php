971e3769854a2aa8c05e5c2f353eccacfe3c6728
Dmitro  Pohrebennyi <149794693+Dimeydimonov@users.noreply.github.com>
2026-05-07 21:24:40 +0300

    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,22 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use App\Http\Controllers\Controller;
 use App\Services\TicketService;
-
 class DashboardController extends Controller
 {
     public function __construct(
         private readonly TicketService $ticketService
     ) {}
-
     public function index()
     {
         $statsDay = $this->ticketService->getStatistics('day');
         $statsWeek = $this->ticketService->getStatistics('week');
         $statsMonth = $this->ticketService->getStatistics('month');
-
         return view('admin.dashboard', compact('statsDay', 'statsWeek', 'statsMonth'));
     }
 }
