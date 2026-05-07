a83deebcbc51771b08add033134c5f4b92487927
Dmitro  Pohrebennyi <149794693+Dimeydimonov@users.noreply.github.com>
2026-05-07 21:27:09 +0300

    Update CheckRole.php

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -1,11 +1,9 @@
 <?php
 
 namespace App\Http\Middleware;
-
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 class CheckRole
 {
     public function handle(Request $request, Closure $next, ...$roles): Response
@@ -13,11 +11,9 @@ class CheckRole
         if (! $request->user()) {
             return redirect()->route('login');
         }
-
         if (array_any($roles, fn ($role) => $request->user()->hasRole($role))) {
             return $next($request);
         }
-
         abort(403, 'Доступ запрещен');
     }
 }
